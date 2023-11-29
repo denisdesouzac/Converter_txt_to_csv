09e5daa25d4a311d37bb935141f71f3bb6a46b6e
Denis de Souza Cordeiro <denissouza9900@gmail.com>
2023-11-29 14:54:58 -0300

    Criado a função de conversão de txt para json

---
+++ converter.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+// Função para converter TXT para JSON
+function txtToJson(txtFilePath, jsonFilePath) {
+    const data = fs.readFileSync(txtFilePath, 'utf-8');
+    const lines = data.split('\n');
+
+    const jsonData = [];
+    let currentData = {};
+
+    lines.forEach(line => {
+        if (line.includes('************************')) {
+            // Se encontramos uma linha de asteriscos, adicionamos os dados ao array e reiniciamos o objeto
+            jsonData.push(currentData);
+            currentData = {};
+        } else {
+            // Se não for uma linha de asteriscos, tentamos extrair a chave e o valor
+            let [key, value] = line.split(':');
+            if (!value) {
+                [key, value] = line.split('=');
+            }
+
+            if (key && value) {
+                currentData[key.trim()] = value.trim();
+            }
+        }
+    });
+
+    // Adicionamos o último bloco de dados ao array (caso o arquivo não termine com uma linha de asteriscos)
+    if (Object.keys(currentData).length > 0) {
+        jsonData.push(currentData);
+    }
+
+    fs.writeFileSync(jsonFilePath, JSON.stringify(jsonData, null, 2));
+    console.log('TXT to JSON conversion completed.');
+}
+
+// Exemplo de uso
+const txtFile = 'report.txt';
+const jsonFile = 'output.json';
+
+// Convertendo TXT para JSON
+txtToJson(txtFile, jsonFile);
